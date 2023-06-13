2684aa22aaabf3ecfb5908bdc0c2358c38d1defb
Kevin Aragon <aragoncodes@gmail.com>
2023-06-13 12:42:20 +0200

    initial commit

---
+++ src/test/java/com/example/springbootpostgrestutorials/SpringbootPostgresTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootpostgrestutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostgresTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
